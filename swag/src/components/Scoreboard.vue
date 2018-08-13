5eeb44ee8335af33bf3cf01ec5f8f4e9fa51b739
Luca Venier <luca.venier@rwth-aachen.de>
2018-08-13 17:35:06 +1000

    Something changed for sure.

--- swag/src/components/Scoreboard.vue
+++ swag/src/components/Scoreboard.vue
@@ -2,7 +2,7 @@
   <div class="hello">
       <div class="container d-flex flex-column align-items-center ">
 
-      <h2>Hey Dude, you achieved <span class="text-success">{{score}}</span> points! Therefore your total sums up to 1460 points.</h2>
+      <h2>Hey Dave, you achieved <span class="text-success">{{score}}</span> points! Therefore your total sums up to 1460 points.</h2>
       
       <router-link class="btn btn-primary btn-lg" to="/">Leave Game</router-link>
       
